c14a1418e224cb635c77105882bd50fc4e0da382
Carman Fu <huivioemp1bs@posteo.com>
2026-06-20 02:01:11 +0800

    feat(grammar & tests): init

--- grammar.js
+++ grammar.js
@@ -7,11 +7,55 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+const newlines = /(\r?\n)+/
+
+const sepBy1 = (sep, a) => seq(
+  repeat(seq(a, sep)),
+  a
+)
+const sepBy = (sep, a) => optional(sepBy1(sep, a))
+
+// the "inclusive or": a | b | seq(a, b)
+const or = (a, b) => choice(a, b, seq(a, b))
+
 export default grammar({
-  name: "atob",
+  name: 'atob',
+
+  extras: $ => [
+    $.comment,
+    newlines,
+  ],
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ =>
+      sepBy(newlines, $.rule),
+
+    rule: $ => seq(
+      optional($.rule_keyword),
+      optional($.lhs),
+      '=',
+      optional($.rhs)
+    ),
+
+    lhs: $ => or($.lhs_keyword, $.text),
+
+    rhs: $ => or($.rhs_keyword, $.text),
+
+    lhs_keyword: _ => choice(
+      '(start)',
+      '(end)'
+    ),
+
+    rhs_keyword: _ => choice(
+      '(start)',
+      '(end)',
+      '(return)'
+    ),
+
+    rule_keyword: _ => '(once)',
+
+    text: _ => token(/[^\r\n#=()]+/),
+
+    comment: _ => token(/#.*/),
   }
 });
