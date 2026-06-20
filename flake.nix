792ee5bca358afe40c50dcbd290d980529527c24
Carman Fu <huivioemp1bs@posteo.com>
2026-06-20 17:00:03 +0800

    feat(nix): make nvim-compatible grammar an individual package

--- flake.nix
+++ flake.nix
@@ -13,15 +13,19 @@
       let
         pkgs = import nixpkgs { inherit system; };
         grammar = import ./default.nix { inherit pkgs; };
+        nvim-compatible = pkgs.vimPlugins.nvim-treesitter.grammarToPlugin grammar;
       in
       {
-        packages.default = grammar;
+        packages = {
+          default = grammar;
+          inherit nvim-compatible;
+        };
         devShells.default = import ./shell.nix { inherit pkgs; };
         overlays.default = _: prev: {
           vimPlugins = prev.vimPlugins // {
             nvim-treesitter = prev.vimPlugins.nvim-treesitter // {
               grammarPlugins = prev.vimPlugins.nvim-treesitter.grammarPlugins // {
-                atob = prev.vimPlugins.nvim-treesitter.grammarToPlugin grammar;
+                atob = nvim-compatible;
               };
             };
           };
